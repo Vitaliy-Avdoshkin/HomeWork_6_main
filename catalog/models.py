37daaa82f3c7048550f78c67b891a111de612e6e
AKVA <wioswa@gmail.com>
2024-11-07 19:56:21 +0300

    HW_6_23 create model - Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,30 +1,68 @@
 from django.db import models
 
+
 class Product(models.Model):
-    name = models.CharField(max_lenght=100, verbose_name='Наименование', help_text='Введите наименование продукта')
-    description = models.CharField(max_lenght=100, verbose_name='Описание', help_text='Введите описание продукта')
-    photo = models.ImageField(upload_to='catalog/photo', blank=True, null=True, verbose_name='Фото', help_text='Загрузите фото продукта')
-    category = models.ForeignKey(on_delete=models.SET_NULL, verbose_name='Категория', help_text='Пожалуйста, укажите категорию продукта', null=True, blank=True)
+    name = models.CharField(
+        max_lenght=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.CharField(
+        max_lenght=100, verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    photo = models.ImageField(
+        upload_to="catalog/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.ForeignKey(
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Пожалуйста, укажите категорию продукта",
+        null=True,
+        blank=True,
+        related_name="Product",
+    )
     purchase_price = models.IntegerField()
-    created_at = models.DateField(blank=True, null=True, verbose_name='Дата создания', help_text='Укажите дату создания')
-    updated_at = models.DateField(blank=True, null=True, verbose_name='Дата последнего изменения', help_text='Укажите дату последнего изменения')
+    created_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+    )
+    updated_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения",
+        help_text="Укажите дату последнего изменения",
+    )
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name', 'purchase_price', 'created_at']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "purchase_price", "created_at"]
 
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
-    name = models.CharField(max_lenght=100, verbose_name='Наименование категории', help_text='Введите наименование категории')
-    description = models.CharField(max_lenght=100, verbose_name='Описание категории', help_text='Введите описание категории')
+    name = models.CharField(
+        max_lenght=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.CharField(
+        max_lenght=100,
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+    )
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
