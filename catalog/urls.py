99864ee81bce68bf644845e6b12e75cb9befef07
AKVA <wioswa@gmail.com>
2024-11-30 00:33:08 +0300

    HW_6_24 is DONE

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 
-# from catalog.views import products_list
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_details
+
 
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("product/<int:pk>/", product_details, name="product_details"),
     path("contacts/", contacts, name="contacts"),
+
 ]
