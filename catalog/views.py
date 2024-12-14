34cfe92ce97b5b372671da330448ccfb98a543fb
AKVA <wioswa@gmail.com>
2024-12-14 15:57:55 +0300

    HW_6_27 is DONE

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -20,7 +20,7 @@ class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
